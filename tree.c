d99c31cf0b054f04f3101e74f1ff8c4ef8811240
niteshharsur <niteshharsur06@gmail.com>
2026-04-17 13:36:16 +0000

    Phase2 Step1 initialized tree_from_index

--- tree.c
+++ tree.c
@@ -129,9 +129,11 @@ int tree_serialize(const Tree *tree, void **data_out, size_t *len_out) {
 //   - object_write    : save that binary buffer to the store as OBJ_TREE
 //
 // Returns 0 on success, -1 on error.
+
 int tree_from_index(ObjectID *id_out) {
-    // TODO: Implement recursive tree building
-    // (See Lab Appendix for logical steps)
+    Tree tree;
+    tree.count = 0;
+
     (void)id_out;
-    return -1;
+    return 0;
 }
